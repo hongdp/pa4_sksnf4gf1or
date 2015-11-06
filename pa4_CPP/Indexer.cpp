af45645275c192fedfb66ce357cebe37944c504e
KeKe <pingjinhao@0587343396.wireless.umich.net>
2015-11-05 20:11:22 -0500

    stop_words

--- pa4_CPP/Indexer.cpp
+++ pa4_CPP/Indexer.cpp
@@ -1,6 +1,6 @@
 #include "Indexer.h"
-
 #include <fstream>
+#include <iostream>
 
 using std::ifstream;
 using std::ostream;
@@ -11,4 +11,21 @@ void Indexer::index(ifstream& content, ostream& outfile)
 {
     // Fill in this method to parse the content and build your
     // inverted index file.
+
+
+}
+
+Indexer::Indexer()
+{
+    ifstream myFile;
+    myFile.open("stop_words.txt");
+    std::string word;
+    if (myFile.is_open()) {
+      while (myFile >> word) {
+        //add word to set
+        stop_words.insert(word);
+        //cout << word<< endl;
+      }
+    }
+    myFile.close();
 }
